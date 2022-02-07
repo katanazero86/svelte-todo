4630888832d391f9a264a636a2a2aad40b62c174
z1004man@naver.com <z1004man@naver.com>
2022-02-08 02:40:57 +0900

    컴포넌트 작성 - TodoTab.svelte 컴포넌트 작성 - CSS 수정 및 간격 조정 - TodoAddBtn.svelte 컴포넌트 작성

--- src/components/todo/Todo.svelte
+++ src/components/todo/Todo.svelte
@@ -3,12 +3,13 @@
     import Today from "../today/Today.svelte";
     import Checkbox from "../checkbox/Checkbox.svelte";
     import TodoTab from "../tabs/TodoTab.svelte";
+    import TodoAddBtn from "../buttons/TodoAddBtn.svelte";
 
     let label: string = '보훈님 회의';
 </script>
 
 
-<div class="bg-slate-50 w-[550px] rounded-md py-12 shadow-md">
+<div class="bg-slate-50 w-[550px] h-[600px] relative rounded-md py-12 shadow-md">
     <div class="pb-4">
         <Today/>
     </div>
@@ -18,4 +19,16 @@
     <div class="h-80 max-h-full overflow-auto px-5">
         <Checkbox label={label}/>
     </div>
+    <div class="todo-btn">
+        <TodoAddBtn />
+    </div>
 </div>
+
+<style>
+    .todo-btn {
+        position: absolute;
+        left: 50%;
+        bottom: -70px;
+        transform: translate(-50%, -50%);
+    }
+</style>
