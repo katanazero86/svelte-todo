a46cf11db0b0d33e545604b693d699ffc0a50938
z1004man@naver.com <z1004man@naver.com>
2022-02-07 11:51:27 +0900

    컴포넌트 작성 - Checkbox.svelte 트랜지션 적용(fade in / out)

--- src/components/checkbox/Checkbox.svelte
+++ src/components/checkbox/Checkbox.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import {fade} from 'svelte/transition';
+
     let checked = false;
     export let label;
 </script>
@@ -6,10 +8,13 @@
 <label class="checkbox">
     <input type="checkbox" bind:checked={checked}/>
     <span class="checkbox__check">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="18" height="18">
-      <path stroke="currentColor" fill="currentColor" fill-rule="nonzero" stroke-width="1.5"
-            d="M8.489 13.597l7.304-7.304a1 1 0 0 1 1.414 1.414l-8 8a1 1 0 0 1-1.403.011l-4-3.875a1 1 0 1 1 1.392-1.436l3.293 3.19z"></path>
-    </svg>
+        {#if checked}
+            <svg transition:fade={{duration: 200}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="15"
+                 height="15">
+        <path stroke="currentColor" fill="currentColor" fill-rule="nonzero" stroke-width="1.5"
+              d="M8.489 13.597l7.304-7.304a1 1 0 0 1 1.414 1.414l-8 8a1 1 0 0 1-1.403.011l-4-3.875a1 1 0 1 1 1.392-1.436l3.293 3.19z"></path>
+        </svg>
+            {/if}
     </span>
     <p class="checkbox__label">{label}</p>
 </label>
@@ -27,25 +32,22 @@
         height: 0;
     }
 
-    .checkbox > input[type="checkbox"]:checked + .checkbox__check > svg {
-        color: #6366f1;
-    }
+    /*.checkbox > input[type="checkbox"]:checked + .checkbox__check > svg {*/
+    /*    color: #6366f1;*/
+    /*}*/
 
     .checkbox > .checkbox__check {
-        display: inline-block;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         border: 2px solid #dbdbdb;
         width: 20px;
         height: 20px;
         border-radius: 5px;
-        position: relative;
     }
 
     .checkbox__check > svg {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        color: transparent;
+        color: #6366f1;
     }
 
     .checkbox > .checkbox__label {
