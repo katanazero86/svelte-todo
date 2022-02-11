4cbf93efd23fbcb928df40717e8b2951eeb5c066
zero86 <katanazero86@gmail.com>
2022-02-11 12:43:08 +0900

    컴포넌트 작성 및 파이어베이스 연동 - CSS 수정 - mobile hover 이슈 처리

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    screens: {
+      touch: {'raw': 'hover: none'},
+      'can-hover': {'raw': 'hover: hover'},
+    }
   },
   plugins: [],
 }
